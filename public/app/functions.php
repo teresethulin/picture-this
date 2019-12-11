43691e6d4a964064132224ae1337410bba31182c
teresethulin <terese.thulin@gmail.com>
2019-12-11 19:47:07 +0100

    Create function newUser()

--- public/app/functions.php
+++ public/app/functions.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+
 if (!function_exists('redirect')) {
     /**
      * Redirect the user to given path.
@@ -14,5 +15,40 @@ if (!function_exists('redirect')) {
     {
         header("Location: ${path}");
         exit;
+    }
+}
+
+
+function newUser()
+{
+    $config = require __DIR__ . '/config.php';
+    $pdo = new PDO($config['database_path']);
+
+    // Fetch all users
+    $statement = $pdo->query('SELECT * FROM user');
+    $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    if (isset($_POST['username'], $_POST['full-name'], $_POST['email'], $_POST['password'])) {
+        $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $fullName = filter_var($_POST['full-name'], FILTER_SANITIZE_STRING);
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $dateCreated = date("Y-m-d H:i:s");
+
+        $query = "INSERT INTO picture_this (username, full_name, email, password, date_created) VALUES ('$username', '$fullName', '$email', '$password', '$dateCreated')";
+
+        $statement = $pdo->prepare($query);
+
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+
+        $statement->bindParam(':username', $username, PDO::PARAM_STR);
+        $statement->bindParam(':full-name', $fullName, PDO::PARAM_STR);
+        $statement->bindParam(':email', $email, PDO::PARAM_STR);
+        $statement->bindParam(':password', $password, PDO::PARAM_STR);
+        $statement->bindParam(':date', $dateCreated, PDO::PARAM_INT);
+
+        $statement->execute();
     }
 }
