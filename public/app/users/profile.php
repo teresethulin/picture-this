fa2940f752ac3d6638ac6940096a3614be792285
teresethulin <terese.thulin@gmail.com>
2019-12-17 10:39:26 +0100

    Check if isLoggedIn in profile before echoing userdata

--- public/app/users/profile.php
+++ public/app/users/profile.php
@@ -1,15 +1,19 @@
 <?php require __DIR__ . '/../../views/header.php'; ?>
 
-<h1>
-    <?php echo $_SESSION['user']['username']; ?>
-</h1>
+<?php if (isLoggedIn()) : ?>
 
-<p>
-    <?php echo $_SESSION['user']['full_name']; ?>
-</p>
+    <h1>
+        <?php echo $_SESSION['user']['username']; ?>
+    </h1>
 
-<p>
-    <?php echo $_SESSION['user']['biography']; ?>
-</p>
+    <p>
+        <?php echo $_SESSION['user']['full_name']; ?>
+    </p>
+
+    <p>
+        <?php echo $_SESSION['user']['biography']; ?>
+    </p>
+
+<?php endif; ?>
 
 <?php require __DIR__ . '/../../views/footer.php'; ?>
