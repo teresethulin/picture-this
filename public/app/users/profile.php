ead37dc1f7ed2fe1de25142865d43508bb5b4e5c
teresethulin <terese.thulin@gmail.com>
2019-12-17 10:31:50 +0100

    Update profile.php

--- public/app/users/profile.php
+++ public/app/users/profile.php
@@ -4,6 +4,12 @@
     <?php echo $_SESSION['user']['username']; ?>
 </h1>
 
+<p>
+    <?php echo $_SESSION['user']['full_name']; ?>
+</p>
 
+<p>
+    <?php echo $_SESSION['user']['biography']; ?>
+</p>
 
 <?php require __DIR__ . '/../../views/footer.php'; ?>
