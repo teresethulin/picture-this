ecc897853aa754a9cabc86e6597f0636dd92bf34
teresethulin <terese.thulin@gmail.com>
2019-12-20 17:27:06 +0100

    A user is now able to update their full name

---
+++ public/app/users/name.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../views/header.php';
+
+// Redirect the user if not logged in
+if (!isLoggedIn()) {
+    redirect('/');
+}
+
+// User
+$userID = $_SESSION['user']['id'];
+$user = getUserById($userID, $pdo);
+
+// Display messages
+$errors = [];
+$successes = [];
+
+
+// UPDATE USER FULL NAME
+if (isset($_POST['full_name'])) {
+
+    $name = filter_var($_POST['full_name'], FILTER_SANITIZE_STRING);
+
+    $query = 'UPDATE user SET full_name = :name WHERE id = :userid';
+    $statement = $pdo->prepare($query);
+
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $statement->bindParam(':name', $name, PDO::PARAM_STR);
+    $statement->execute([
+        ':name' => $name,
+        ':userid' => $userID,
+    ]);
+
+    // Display confirmation
+    $successes[] = "Name updated.";
+
+    if (count($successes) > 0) {
+
+        $_SESSION['successes'] = $successes;
+        redirect('/edit-profile.php');
+        exit;
+    }
+}
