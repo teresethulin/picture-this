4173a3b61812f68ec98e4170c433d5e1209dca46
teresethulin <terese.thulin@gmail.com>
2019-12-29 16:12:16 +0100

    Add avatar.css to header.php, avatar width+height+border-radius

--- public/views/header.php
+++ public/views/header.php
@@ -18,6 +18,7 @@ require __DIR__ . '/../app/successes.php';
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/styles/main.css">
+    <link rel="stylesheet" href="/assets/css/avatar.css">
 </head>
 
 <body>
