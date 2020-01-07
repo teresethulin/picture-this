7a829a55456649497d3b3fd8750e5ff52c1ad3ff
teresethulin <terese.thulin@gmail.com>
2020-01-07 10:46:10 +0100

    Add icons to nav

--- public/views/navigation.php
+++ public/views/navigation.php
@@ -4,19 +4,19 @@
     <ul class="navbar-nav">
         <?php if (isLoggedIn()) : ?>
             <li class="nav-item">
-                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php"><i class="fas fa-camera-retro"></i></a>
             </li><!-- /nav-item -->
 
             <li class="nav-item">
-                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/profile.php' ? 'active' : ''; ?>" href="/../app/users/profile.php">Profile</a>
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/profile.php' ? 'active' : ''; ?>" href="/../app/users/profile.php"><i class="far fa-user"></i></a>
             </li><!-- /nav-item -->
 
             <li class="nav-item">
-                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/upload.php' ? 'active' : ''; ?>" href="/upload.php">Upload</a>
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/upload.php' ? 'active' : ''; ?>" href="/upload.php"><i class="fas fa-plus"></i></a>
             </li><!-- /nav-item -->
 
             <li class="nav-item">
-                <a class="nav-link" href="/app/users/logout.php">Logout</a>
+                <a class="nav-link" href="/app/users/logout.php"><i class="fas fa-sign-out-alt"></i></a>
             </li>
         <?php else : ?>
             <li>
